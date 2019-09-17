1790943f04a70a9301caf2099c4c0278c3cac172
amakUfl <a.makarevitch@ufl.edu>
2019-09-16 21:34:52 -0400

    persisting listings into DB

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -13,6 +13,28 @@ var fs = require('fs'),
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
 
+// helper function to connect to the MongoDB
+async function establishConnection() {
+  try {
+    await mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    // clearing existing records in the collection
+      .then(async () => {
+        var collection = mongoose.connection.useDb(config.db.database).collection(config.db.collection);
+        await collection.deleteMany({});
+      // handling error during connection attempt
+      }).catch(() => {
+        console.error(error);
+      });
+    // handling error with connection
+    mongoose.connection.on('error', err => {
+      console.error(err);
+    });
+  // handling error with connection definition
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
@@ -20,6 +42,22 @@ var fs = require('fs'),
 
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
+fs.readFile(config.document.uri, config.document.encoding, async function(err, data) {
+  // handling error with reading file
+  if (err) console.error(err);
+  // parsing data into Object
+  data = JSON.parse(data);
+  // connecting to the MongoDB
+  await establishConnection();
+  // when connected - iterate through all entries
+  data.entries.forEach(listing => {
+    // and write them using custom Schema
+    new Listing(listing).save(function(err, listing) {
+      // handle error during writing document
+      if (err) console.error(err);
+    });
+  });
+});
 
 
 /*  
